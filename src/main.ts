7077ded1d9605ba19ad3ed86838365c780c83e29
piotr <piotrek.priv.michalski@wp.pl>
2023-03-15 23:33:01 +0100

    Credentials/cookies working correctly

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-
+import * as session from 'express-session';
 const cookieSession = require('cookie-session');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: 'http://localhost:5173',
+    credentials: true,
   });
   app.use(
     cookieSession({
